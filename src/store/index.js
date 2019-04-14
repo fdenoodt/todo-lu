5645ba2b5d03c4b476116711572fbac24cb98420
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-14 16:18:36 +0200

    bootstrap-vue

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
