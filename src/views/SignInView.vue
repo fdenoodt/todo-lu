7be7de42e0276bd9ed1782e68c205b166cb03f38
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-14 16:09:19 +0200

    signinview

---
+++ src/views/SignInView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Sign in</p>
+    <!-- <p>{{user.email}}</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignInView",
+  methods: {
+    test() {}
+  },
+  created() {
+    this.test();
+  }
+};
+</script>
