4689f71e0ae9f2934d3cfef3594e3438295a41b6
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-14 16:16:00 +0200

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import Router from './router'
+import Store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
+  Store,
+  Router,
   render: h => h(App),
 }).$mount('#app')
