56a8e90e1b3957fa2548939763212a0a8fa47d22
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-16 19:11:42 +0200

    registerview

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     Create an account
+    <Form v-on:form-submit="submit"></Form>
+    <p>
+      <router-link to="/SignInView">Register</router-link>
+    </p>
   </div>
 </template>
 
 <script>
+import Form from "../components/Form";
+import { mapActions } from "vuex";
+
 export default {
-  name: "RegisterView"
-}
+  name: "RegisterView",
+  components: {
+    Form
+  },
+  methods: {
+    ...mapActions(["register"]),
+    submit(email, password) {
+      this.register({ email, password });
+    }
+  }
+};
 </script>
