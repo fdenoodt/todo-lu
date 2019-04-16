6fc566b28e04d87c447e97a62f10a2a7eaa8df3e
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-16 17:53:41 +0200

    firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAq4BDWj4P8joAh3FiCP2ZGs4QMdX4qAeg",
+  authDomain: "tasks-d6816.firebaseapp.com",
+  databaseURL: "https://tasks-d6816.firebaseio.com",
+  projectId: "tasks-d6816",
+  storageBucket: "tasks-d6816.appspot.com",
+  messagingSenderId: "226958497004"
+};
+firebase.initializeApp(config);
