a4d9386b5a96eaf5d4c4fb20113130c3cda77b7d
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-16 19:11:10 +0200

    account form for signin + register

---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-input v-bind:value="email" v-model="email"/>
+    <b-input v-bind:value="password" v-model="password"/>
+    <b-button @click="$emit('form-submit', email, password)">Submit</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  components: {},
+  data() {
+    return {
+      email: "em",
+      password: "pw"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-input v-bind:value="content" v-model="content"/>
+</template>
+
+<script>
+
+export default {
+  name: "InputField",
+  props: ["content"]
+};
+</script>
+
+<style>
+</style>
