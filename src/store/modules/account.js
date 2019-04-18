38a24f0d700deb288443b65d4dac42f8d0bb2ca9
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-18 18:34:49 +0200

    retrieve firebase data

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -22,6 +22,22 @@ const actions = {
         if (response) {
           commit('setUser', response)
           router.push("/HomeView")
+
+          db.collection('users')
+            .doc(response.uid)
+            .get()
+            .then((doc) => {
+              if (doc.exists) {
+                const data = doc.data()
+                commit('setNotes', data.notes)
+                commit('setReminders', data.reminders)
+                commit('setTasks', data.tasks)
+              }
+              else {
+                actions.loadEmptyUserDataInFireStore()
+                //Todo: when user didnt have data and signs in. this method is called, but the data isn't loaded into ram. only into firestore
+              }
+            })
         }
         else {
           commit('setUser', null)
@@ -38,7 +54,7 @@ const actions = {
       .auth()
       .signInWithEmailAndPassword(email, pw)
       .catch(() => {
-        console.log("error")
+        console.error("error")
       })
   },
   register({ commit }, user) {
@@ -49,26 +65,35 @@ const actions = {
       .auth()
       .createUserWithEmailAndPassword(email, pw)
       .then(() => {
-        const uid = firebase.auth().currentUser.uid
-        db.collection("users")
-          .doc(uid)
-          .set({
-            tasks: [],
-            reminders: [],
-            notes: []
-          })
+        actions.loadEmptyUserDataInFireStore()
       })
       .catch((ex) => {
-        console.log("error", ex)
+        console.error("error", ex)
       });
   },
+  loadEmptyUserDataInFireStore() {
+    const uid = firebase.auth().currentUser.uid
+    db.collection("users")
+      .doc(uid)
+      .set({
+        tasks: [],
+        reminders: [],
+        notes: []
+      })
+      .catch((ex) => {
+        console.error("error", ex)
+      })
+  },
   signOut({ commit }) {
-    firebase.auth().signOut();
+    firebase.auth().signOut()
   }
 }
 
 const mutations = {
   setUser: (state, u) => (state.user = u),
+  setTasks: (state, t) => (state.tasks = t),
+  setReminders: (state, r) => (state.reminders = r),
+  setNotes: (state, n) => (state.notes = n)
 }
 
 export default {
