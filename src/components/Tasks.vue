327eeb17bef6f5cdf74df1ee263d5d8be70ef8ea
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-04-19 17:23:47 +0200

    tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-bind:key="task.id" v-for="task in getTasks">
+      <TaskItem v-bind:task="task"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import TaskItem from "./TaskItem.vue";
+
+export default {
+  name: "Tasks",
+  components: {
+    TaskItem
+  },
+  computed: {
+    ...mapGetters(["getTasks"])
+  }
+};
+</script>
+
+<style>
+</style>
